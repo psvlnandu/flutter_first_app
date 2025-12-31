997fa54f30ce9c2eb0b402af30c37eaff421e742
psvl nandu <nandu@embedsense.com>
2025-12-30 21:59:33 -0600

    snack bars & ListView.Builder

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,14 @@ Scaffold Widget
 
 */
 // immutable
-  class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(home: ScaffoldExample());
   }
 }
+
 // immutable
 class ScaffoldExample extends StatefulWidget {
   const ScaffoldExample({super.key});
@@ -30,16 +31,54 @@ class ScaffoldExample extends StatefulWidget {
 // mutable
 class _ScaffoldExampleState extends State<ScaffoldExample> {
   int _count = 0;
-  final List _drinks = ['Matcha Latte', 'Cortado', 'Hot Chocolate'];
-
+  final List _drinks = ['Matcha Latte', 'Cortado', 'Flat White', 'Cappuccino', 'Espresso', 'Hot Chocolate'];
+  final List _drink_imgs = [];
+  final List _out_of_order = ['Matcha Latte', 'Hot Chocolate'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Old  Coffee')),
-      body: Center(child: Text('you have selected ${_drinks[_count % _drinks.length]} ')),
+      appBar: AppBar(title: const Text('Old Market Coffee')),
+      body: ListView.builder(
+        itemCount: _drinks.length,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: const EdgeInsets.all(15),
+            child: ListTile(
+              leading: const Icon(Icons.coffee),
+              title: Text(_drinks[index]),
+              onTap: () {
+                if (_out_of_order.contains(_drinks[index])) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Oops! ${_drinks[index]} is currently out of stock.',
+                      ),
+                      backgroundColor: Colors.brown[400],
+                      behavior: SnackBarBehavior.floating,
+                    ),
+                  );
+                }else{
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        ' ${_drinks[index]} added to your cart.',
+                      ),
+                      backgroundColor: Colors.pink[400],
+                      behavior: SnackBarBehavior.floating,
+                      
+                    ),
+                  );
+                }
+              },
+            ),
+          );
+        },
+      ),
+      // Center(child: Text('you have selected ${_drinks[_count % _drinks.length]} ')),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() => _count++), // telling the class to re build
+        onPressed: () =>
+            setState(() => _count++), // telling the class to re build
         tooltip: 'Increment Counter',
         child: const Icon(Icons.next_plan),
       ),
