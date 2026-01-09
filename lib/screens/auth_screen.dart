efb32c044342882b05803f693c1f19a41aec7929
psvl nandu <nandu@embedsense.com>
2026-01-08 23:14:41 -0600

    AuthScreen using Checkout screen layout

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widgets/drawer/signIn_form.dart';
 import 'package:flutter_application_1/widgets/drawer/signup_form.dart';
@@ -13,25 +12,45 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Account"),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: "Sign In"),
-              Tab(text: "Sign Up"),
-            ],
-            indicatorColor: Colors.brown,
-            labelColor: Colors.brown,
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Checkout'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Theme(
+        data: Theme.of(context).copyWith(
+          // Applies Coolvetica only to the body children
+          textTheme: Theme.of(
+            context,
+          ).textTheme.apply(fontFamily: 'coolvetica'),
         ),
-        body: const TabBarView(
-          children: [
-            SignInForm(),
-            SignUpForm(),
-          ],
+        child: Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: 600,
+            ), // Standard web width
+            child: TabBarView(
+              children: [
+                // Tab 1: Sign In
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24.0,
+                    vertical: 20,
+                  ),
+                  child: SignInForm(), 
+                ),
+                // Tab 2: Sign Up
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24.0,
+                    vertical: 20,
+                  ),
+                  child: SignUpForm(), 
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
